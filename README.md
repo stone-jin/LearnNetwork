713f56341d7bd5421e65ec1629ab634d8d102d5f
jinbing <1520006273@qq.com>
2014-05-14 13:56:36 +0800

    2014年5月14日版本1

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-LearnNetwork
-============
+### LearnNetwork
+###### android学习的小例子
+###### 先熟练掌握android方面的技术，再向外扩张技术层次
+
+
+### 2014年5月14日
+```
+版本1
+1.新建项目com.meibing.test1
+2.com.meibing.test1用于android网络变成学习
+```
---
+++ stoneLearn/src/com/meibing/test1/Test1.java
@@ -0,0 +1,16 @@
+package com.meibing.test1;
+
+import com.meibing.eoetest1.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test1 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test1);
+	}
+}
